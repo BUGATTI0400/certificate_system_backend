7adcecc1bf8102942140c480e91ed8902f6599ae
Kadry <m.kadry2468.3@gmail.com>
2026-06-09 00:05:24 +0300

    Remove condition

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 })
 .catch(err => console.error('Mongo connection error', err));
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log('Server running on', PORT));
-}
+app.listen(PORT, () => console.log('Server running on', PORT));
 
 module.exports = app;
